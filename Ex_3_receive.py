683d9d772b54eb3ff755083df9c36c3484396c61
Szczpan <01168997@pw.edu.pl>
2023-01-16 09:50:03 +0100

    bind ip

--- Ex_3_receive.py
+++ Ex_3_receive.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-
+sock.bind(("192.168.114.193", 21054))
 count = 0
 while True:
     bufsiz, address = sock.recvfrom(65507)
