cd2fb61bfcaf5c43cee0854d88f1b418619f4359
Szczpan <01168997@pw.edu.pl>
2023-01-16 09:56:18 +0100

    acccept data

--- Ex_3_receive.py
+++ Ex_3_receive.py
@@ -3,8 +3,10 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind(("192.168.114.193", 21054))
 count = 0
+sock.listen(1)
+sock.accept()
 while True:
-    data, address = sock.recvfrom(65507)
+    data, address = sock.recv(65507)
     print(data.decode('utf-8'))
     if address == "192.168.114.122":
         count += 1
