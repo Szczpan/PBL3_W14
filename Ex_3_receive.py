6520d918b347d4145fc26cae1b52d23ee6b308d5
Szczpan <01168997@pw.edu.pl>
2023-01-16 09:51:54 +0100

    print data

--- Ex_3_receive.py
+++ Ex_3_receive.py
@@ -4,6 +4,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind(("192.168.114.193", 21054))
 count = 0
 while True:
-    bufsiz, address = sock.recvfrom(65507)
-    if bufsiz == 65507 and address == "192.168.114.122":
+    data, address = sock.recvfrom(65507)
+    print(data.decode('utf-8'))
+    if address == "192.168.114.122":
         count += 1
