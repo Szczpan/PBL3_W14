aaef6480d7d95bb0f773af57fb96205d98ee383f
Szczpan <01168997@pw.edu.pl>
2023-01-16 10:08:54 +0100

    update

--- Ex_2.py
+++ Ex_2.py
@@ -9,11 +9,12 @@ MESSAGE = ""
 for i in range(65507):
     MESSAGE += random.choice(string.ascii_letters)
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.connect((UDP_IP, UDP_PORT))
 while True:
     packet_n = input("Podaj liczbę pakietów: ")
     packet_interval = input("Podaj okres nadawania w milisekundach: ")
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
     for i in range(int(packet_n)):
         sleep(int(packet_interval)/1000)
-        sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+        sock.send(bytes(MESSAGE, "utf-8"))
